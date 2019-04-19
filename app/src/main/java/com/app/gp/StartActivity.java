d38c971a990e0a9347f29a3d664df6e2c67ddf20
wangjunqiang <junqiang.wang@aiyingli.com>
2019-04-19 22:37:34 +0800

    sfdsfd

--- app/src/main/java/com/app/gp/StartActivity.java
+++ app/src/main/java/com/app/gp/StartActivity.java
@@ -1,13 +1,23 @@
 package com.app.gp;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class StartActivity extends AppCompatActivity {
 
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
